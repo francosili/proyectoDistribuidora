f268443abbf18e557bb845fb4ca0ab27b9b25f30
francosili <fsili72@gmail.com>
2017-09-19 17:00:22 -0300

    cambios en estilos

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -7,7 +7,7 @@ export class AuthService {
   constructor(public http: Http) {}
 
   getCategorias() {
-    return this.http.get('http://192.168.0.100:8080/DistribuidoraApi/webresources/entidades.categoria');
+    return this.http.get('http://192.168.0.100:8080/DistribuidoraApi/webresources/entidades.categoria/todos');
   }
 
   getArticulos(idVendedor?) {
